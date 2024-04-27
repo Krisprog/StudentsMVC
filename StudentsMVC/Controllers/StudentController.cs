e6d7df0d0bfe457bdf72c3992d56f9e69b0c113e
cherkezyan <cherkezyan@itstep.academy>
2024-04-27 17:14:06 +0300

    Add project files.

---
+++ StudentsMVC/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentsMVC
+{
+    public class StudentController : Controller
+    {
+        StudentContext db;
+        public StudentController(StudentContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Student> students = await Task.Run(() => db.Students);
+            ViewBag.Students = students;
+            ViewData["Students"] = students;
+            return View();
+        }
+    }
+}
